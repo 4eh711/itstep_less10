59cdd69d85c43e4a7ee6245d63ed896304ee7a26
4eh711 <Cheh711@gmail.com>
2023-09-09 02:50:54 +0300

    Work with DB_2

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ import sqlite3
 
 connection = sqlite3.connect("itstep_DB.sl3",5)
 cur = connection.cursor()
-cur.execute("CREATE TABLE first_table (name Text);")
+#cur.execute("DROP TABLE first_table;")
+cur.execute("INSERT INTO first_table (name, temperature) VALUES ('Kremenchyg','22');")
+#cur.execute("CREATE TABLE first_table (name TEXT, temperature TEXT);")
+#cur.execute("DELETE FROM first_table WHERE rowid=1;")
+#cur.execute("SELECT rowid, name FROM first_table WHERE rowid = 3;")
 connection.commit()
+res = cur.fetchall()
+print(res)
 connection.close()
